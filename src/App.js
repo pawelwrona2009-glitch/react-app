0b3956628638225f3c77d67a0399acfbe620bef0
Paweł W <pawelwrona2009@gmail.com>
2025-11-22 11:03:53 +0100

    React routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import Posts from "./pages/Posts";
+import Login from "./pages/login/Login";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={ <Posts/>}></Route>
       <Route path="posts" element={ <Posts/>}></Route>
+      <Route path="login" element={ <Login/>}></Route>
     </Routes>
     </BrowserRouter>
   );
